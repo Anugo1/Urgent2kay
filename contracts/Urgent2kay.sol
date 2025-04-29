e1719edfed8b89d304b735e8796afa345544db45
anugo1 <ekehumphery@gmail.com>
2025-04-29 15:06:56 +0100

    token done

---
+++ contracts/Urgent2kay.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Urgent2kay is ERC20, Ownable {
+
+    uint256 public constant REWARD_AMOUNT = 100 * 10 ** 18;
+
+     constructor() ERC20("Urgent2kay", "2tk") {}
+
+     function mint(address to, uint256 amount)external onlyOwner {
+        _mint(to, amount);
+     }
+     }
